4e5b5dacf531c14ff4ce4882970fff234492f958
Jobin Jose <jobin.jose928@gmail.com>
2020-04-30 11:21:44 +0530

    Added Header

---
+++ keralacovid19/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles/header.css";
+const Heading = () => {
+  return (
+    <header id="section0">
+      <h1>KERALA COVID19</h1>
+      <div class="arrow">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </header>
+  );
+};
+
+export default Heading;
