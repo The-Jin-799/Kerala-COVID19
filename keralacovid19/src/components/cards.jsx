8e58ed90c7f51493137e7a09b1fe34851d34965b
Jobin Jose <jobin.jose928@gmail.com>
2020-05-03 16:52:57 +0530

    Local API to get data

--- keralacovid19/src/components/cards.jsx
+++ keralacovid19/src/components/cards.jsx
@@ -1,44 +1,82 @@
-import React from "react";
+import React, { Component } from "react";
 import "./styles/cards.css";
-const StateCard = () => {
-  return (
-    <div className="stateCard">
-      <div className="cardheader">
-        <h1>Complete Report</h1>
-      </div>
-      <div className="cards">
-        <div className="row">
-          <div className="column ">
-            <div className="card confirmed">
-              <h3>300</h3>
-              <p>Confirmed</p>
-            </div>
-          </div>
+class StateCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      currentlyActive: "",
+      totalActive: "",
+      totalDeath: "",
+      totalRecovered: "",
+    };
+  }
 
-          <div className="column ">
-            <div className="card recovered">
-              <h3>200</h3>
-              <p>Recovered</p>
-            </div>
-          </div>
+  componentDidMount() {
+    fetch("http://localhost:8000/status/")
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({
+          isLoading: false,
+          currentlyActive: response.currentlyactive,
+          totalActive: response.totalactive,
+          totalDeath: response.totaldeath,
+          totalRecovered: response.totalrecovered,
+        });
+        console.log(response);
+      });
+  }
 
-          <div className="column">
-            <div className="card active">
-              <h3>96</h3>
-              <p>Active</p>
+  render() {
+    return (
+      <div className="stateCard">
+        <div className="cardheader">
+          <h1>Complete Report</h1>
+        </div>
+        <div className="cards">
+          <div className="row">
+            <div className="column ">
+              <div className="card confirmed">
+                <h3>
+                  {this.state.isLoading ? "Loading" : this.state.totalActive}
+                </h3>
+                <p>Confirmed</p>
+              </div>
             </div>
-          </div>
 
-          <div className="column ">
-            <div className="card death">
-              <h3>4</h3>
-              <p>Deaths</p>
+            <div className="column ">
+              <div className="card recovered">
+                <h3>
+                  {this.state.isLoading ? "Loading" : this.state.totalRecovered}
+                </h3>
+                <p>Recovered</p>
+              </div>
+            </div>
+
+            <div className="column">
+              <div className="card active">
+                <h3>
+                  {this.state.isLoading
+                    ? "Loading"
+                    : this.state.currentlyActive}
+                </h3>
+                <p>Active</p>
+              </div>
+            </div>
+
+            <div className="column ">
+              <div className="card death">
+                <h3>
+                  {this.state.isLoading ? "Loading" : this.state.totalDeath}
+                </h3>
+                <p>Deaths</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default StateCard;
