7bcb31bf2bdc0380b179624fbda18ab3873c0c5b
Jobin Jose <jobin.jose928@gmail.com>
2020-05-06 18:11:58 +0530

    Updates

---
+++ precache-manifest.81b8968caac5932e6e8e81d95989e78c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0913a6079a549cc59d8eef919291711e",
+    "url": "/Kerala-COVID19/index.html"
+  },
+  {
+    "revision": "25088e99dadd3675de00",
+    "url": "/Kerala-COVID19/static/css/main.8b3a28d5.chunk.css"
+  },
+  {
+    "revision": "f81e91b033de200a50fc",
+    "url": "/Kerala-COVID19/static/js/2.b01b2376.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Kerala-COVID19/static/js/2.b01b2376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25088e99dadd3675de00",
+    "url": "/Kerala-COVID19/static/js/main.c40a1771.chunk.js"
+  },
+  {
+    "revision": "7b42dbeb97d8ec70e340",
+    "url": "/Kerala-COVID19/static/js/runtime-main.c3f04adc.js"
+  }
+]);
